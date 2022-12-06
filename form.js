f47f249e38b60e0c7d1623b6149a2e266dab7c12
fenger@floydhamilton.nl <fenger@floydhamilton.net>
2022-12-06 12:26:24 +0100

    add pages and fix slider

---
+++ form.js
@@ -0,0 +1,69 @@
+const team = document.querySelector('input[id=team]:checked');
+const teamName = document.querySelector(".team-name-input");
+const formContainer = document.querySelector(".form-container");
+const btnSubmit = document.querySelector(".submit-button");
+const form = document.querySelector("form.submit-form");
+let formHeight = 110;
+
+function animateUp() {
+    if (formHeight >= 2500) {
+      clearInterval(animateFormUp);
+    } else {
+      formHeight = formHeight + 8;
+
+      // this 8 is used for the speed of the animation. the height of the form decreases with 8px per 1/100 second
+      // So speed = (800px per second)
+
+      formContainer.style.maxHeight = formHeight + 'px';
+    };
+  };
+
+function displayForm() {
+  formContainer.classList.remove("form-is-closed");
+  btnSubmit.innerHTML = "Verstuur aanmelding"
+  if (formContainer.classList.contains("hide-form")) {
+    clearInterval(animate);
+  };
+  formContainer.classList.add("show-form");
+  formContainer.classList.remove("hide-form");
+  if (window.innerWidth <= 992) {
+    body.classList.add("no-scroll");
+  };
+  animateFormUp = setInterval(animateUp, 10);
+};
+
+function animateDown() {
+    if (formHeight <= (btnDisplayHeight + btnDisplayMarginBottom)) {
+      form.style.maxHeight = btnDisplayHeight + btnDisplayMarginBottom + 'px';
+      clearInterval(animate);
+      formContainer.classList.remove("show-form");
+    } else {
+      formHeight = formHeight - 8;
+
+      // this 8 is used for the speed of the animation. the height of the form decreases with 8px per 1/100 second
+      // So speed = (800px per second)
+
+      form.style.maxHeight = formHeight + 'px';
+    };
+  };
+
+function hideForm() {
+  clearInterval(animateFormUp);
+  formContainer.classList.add("hide-form");
+  formHeight = form.offsetHeight;
+  if (window.innerWidth <= 992) {
+    body.classList.remove("no-scroll");
+  };
+  animate = setInterval(animateDown, 10);
+
+};
+
+
+
+function showTeam () {
+    teamName.style.display = "flex";
+}
+
+function hideTeam () {
+  teamName.style.display = "none"
+}
--- home.js
+++ home.js
@@ -1,30 +1,96 @@
 const arrowLeft = document.querySelector(".slider-arrow-left");
 const arrowRight = document.querySelector(".slider-arrow-right");
 const cardsContainer = document.querySelector(".cards-container");
+const sliderDots = document.querySelector(".slider-dots");
 const dotOne = document.querySelector(".dot-one");
 const dotTwo = document.querySelector(".dot-two");
+const dotThree = document.querySelector(".dot-three");
+const dots = [dotOne, dotTwo, dotThree];
 const card = document.querySelector(".card-one");
 let transformPx = 0;
+let index = 0;
+let cardWidth = card.offsetWidth;
 
 
-function moveRight () {
-  let cardWidth = card.offsetWidth;
+function moveLeft () {
   transformPx = transformPx + cardWidth + 15;
   let newTransform = transformPx + 'px';
   cardsContainer.style.transform = `translateX(${newTransform})`;
-  dotTwo.classList.remove("active");
-  dotOne.classList.add("active");
-  arrowRight.classList.remove("inactive");
-  arrowLeft.classList.add("inactive");
+  index -= 1;
+
+  console.log(dots[index]);
+
+  dots[index].classList.add("active");
+  for (let i = 0; i < dots.length; i++) {
+    if (i !== index) {
+      dots[i].classList.remove("active");
+    }
+  }
+
+  if (index == 0) {
+    arrowLeft.classList.add("inactive");
+  }
+
+  if (index < dots.length -1) {
+    arrowRight.classList.remove("inactive");
+  }
 }
 
-function moveLeft () {
-  let cardWidth = card.offsetWidth;
+function moveRight () {
   transformPx = transformPx - cardWidth - 15;
   let newTransform = transformPx + 'px';
   cardsContainer.style.transform = `translateX(${newTransform})`;
-  dotOne.classList.remove("active");
-  dotTwo.classList.add("active");
-  arrowLeft.classList.remove("inactive");
-  arrowRight.classList.add("inactive");
+  index += 1;
+
+  dots[index].classList.add("active");
+  for (let i = 0; i < dots.length; i++) {
+    if (i !== index) {
+      dots[i].classList.remove("active");
+    }
+  }
+
+  if (index == dots.length - 1) {
+    arrowRight.classList.add("inactive");
+  }
+
+  if (index > 0) {
+    arrowLeft.classList.remove("inactive");
+  }
 }
+
+function dotClick (e) {
+
+    for (let i = 0; i < dots.length; i++) {
+      if (e.target == dots[i]) {
+        index = i;
+        if (index == 0) {
+          transformPx = 0;
+        } else {
+          transformPx = 0 - (cardWidth + 15) * i;
+        }
+
+        let newTransform = transformPx + 'px';
+        cardsContainer.style.transform = `translateX(${newTransform})`;
+        dots[i].classList.add("active");
+        dots.map(x => {if (x !== e.target) {x.classList.remove("active")}});
+      }
+
+      if (index == 0) {
+        arrowLeft.classList.add("inactive");
+      }
+
+      if (index < dots.length -1) {
+        arrowRight.classList.remove("inactive");
+      }
+
+      if (index == dots.length - 1) {
+        arrowRight.classList.add("inactive");
+      }
+
+      if (index > 0) {
+        arrowLeft.classList.remove("inactive");
+      }
+    }
+}
+
+sliderDots.addEventListener("click", dotClick);
